bd953fd446f93b4e5fe8453fb3bdc88daccc213b
Lanea Lucy S <lanealucy@gmail.com>
2023-08-25 20:46:39 +0200

    added MMDVMHost addon (WIP)

---
+++ MMDVMHost/config.php
@@ -0,0 +1,63 @@
+<?php
+# This is an auto-generated config-file!
+# Be careful, when manual editing this!
+
+date_default_timezone_set('UTC');
+define("MMDVMLOGPATH", "/var/log/mmdvm");
+define("MMDVMINIPATH", "/MMDVMHost");
+define("MMDVMINIFILENAME", "MMDVM.ini");
+define("MMDVMHOSTPATH", "/MMDVMHost");
+define("ENABLEXTDLOOKUP", "on");
+define("TALKERALIAS", "on");
+define("DMRIDDATPATH", "/MMDVMHost/DMRIds.dat");
+define("RESOLVETGS", "on");
+define("YSFGATEWAYLOGPATH", "");
+define("YSFGATEWAYLOGPREFIX", "");
+define("YSFGATEWAYINIPATH", "");
+define("YSFGATEWAYINIFILENAME", "");
+define("YSFHOSTSPATH", "");
+define("YSFHOSTSFILENAME", "");
+define("DMRGATEWAYLOGPATH", "");
+define("DMRGATEWAYLOGPREFIX", "");
+define("DMRGATEWAYINIPATH", "/opt/hotspot/DMRGateway/");
+define("DMRGATEWAYPATH", "/opt/hotspot/DMRGateway/");
+define("DMRGATEWAYINIFILENAME", "DMRGateway.ini");
+define("DAPNETGATEWAYLOGPATH", "");
+define("DAPNETGATEWAYLOGPREFIX", "");
+define("DAPNETGATEWAYINIPATH", "");
+define("DAPNETGATEWAYPATH", "");
+define("DAPNETGATEWAYINIFILENAME", "");
+define("LINKLOGPATH", "");
+define("IRCDDBGATEWAY", "");
+define("TIMEZONE", "Europe/Berlin");
+define("LOCALE", "de_DE");
+define("LOGO", "");
+define("DMRPLUSLOGO", "");
+define("BRANDMEISTERLOGO", "");
+define("REFRESHAFTER", "60");
+define("SHOWRPTINFO", "on");
+define("SHOWMODES", "on");
+define("SHOWLH", "on");
+define("SHOWLOCALTX", "on");
+define("SHOWPROGRESSBARS", "on");
+define("TEMPERATUREALERT", "on");
+define("TEMPERATUREHIGHLEVEL", "80");
+define("SWITCHNETWORKUSER", "");
+define("SWITCHNETWORKPW", "");
+define("VIEWLOGUSER", "admin");
+define("VIEWLOGPW", "");
+define("HALTUSER", "admin");
+define("HALTPW", "");
+define("REBOOTUSER", "admin");
+define("REBOOTPW", "");
+define("RESTARTUSER", "admin");
+define("RESTARTPW", "");
+define("REBOOTYSFGATEWAY", "");
+define("REBOOTMMDVM", "");
+define("REBOOTSYS", "");
+define("HALTSYS", "");
+define("POWERONLINEPIN", "");
+define("POWERONLINESTATE", "");
+define("RSSI", "avg");
+?>
+
---
+++ MMDVMHost/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/with-contenv bashio
+set -e
+
+CONFIG="/MMDVMHost/MMDVM.ini"
+
+bashio::log.info "Creating configuration..."
+
+# Create config
+MMDVM_INI=$(bashio::config 'MMDVM_ini')
+
+#echo ${MMDVM_INI} > "${CONFIG}"
+
+bashio::log.debug ${MMDVM_INI}
+
+
+
+if [ "$(bashio::config 'show_ls')" ]
+then
+	ls -alh /dev/tty*
+fi
+
+
+#php-fpm81 -D
+#lighttpd -f /etc/lighttpd/lighttpd.conf
+
+# Start DHCP server
+bashio::log.info "Starting MMDVMHost..."
+exec /MMDVMHost/MMDVMHost ${CONFIG}
+
+
+#while true; do sleep 2; done
+
+
+
+
